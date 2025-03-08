7cce30e0e69f35484f5242c861a42218486ff6a4
vietvite <vanviet8367@gmail.com>
2025-03-08 16:23:22 +0700

    chore: add secrets.h example

---
+++ src/secrets.h
@@ -0,0 +1,14 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// WiFi credentials
+#define SECRET_WIFI_SSID "your-SSID"
+#define SECRET_WIFI_PASSWORD "your-PASSWORD"
+
+// InfluxDB credentials
+#define SECRET_INFLUXDB_URL "http://192.168.1.100:8086"
+#define SECRET_INFLUXDB_ORG "your-org"
+#define SECRET_INFLUXDB_BUCKET "your-bucket"
+#define SECRET_INFLUXDB_TOKEN "your-token"
+
+#endif // SECRETS_H
